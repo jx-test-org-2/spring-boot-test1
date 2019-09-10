757dbc18eb1a57413d70919da33be37e40f68156
Kenny Younger <kyounger@gmail.com>
2019-09-10 15:58:19 -0500

    Add index controller

--- src/main/java/net/sharpwit/springboottest1/DemoApplication.java
+++ src/main/java/net/sharpwit/springboottest1/DemoApplication.java
@@ -2,6 +2,9 @@ package net.sharpwit.springboottest1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,15 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
+	@RestController
+	public class IndexController {
+		@RequestMapping("/")
+		public String index() {
+			return "Hello";
+		}
+	}
+
 }
+
+
